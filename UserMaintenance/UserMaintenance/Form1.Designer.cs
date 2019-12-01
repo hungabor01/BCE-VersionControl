9fa115e7bc4e9ee89c26d2b08584dd1d559e7506
hungabor01 <hungabor01@gmail.com>
2019-12-01 21:03:57 +0100

    Write list to file

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -32,6 +32,7 @@
       this.btnAdd = new System.Windows.Forms.Button();
       this.lblFullName = new System.Windows.Forms.Label();
       this.txtFullName = new System.Windows.Forms.TextBox();
+      this.btnSave = new System.Windows.Forms.Button();
       this.SuspendLayout();
       // 
       // listUsers
@@ -68,11 +69,22 @@
       this.txtFullName.Size = new System.Drawing.Size(197, 20);
       this.txtFullName.TabIndex = 5;
       // 
+      // btnSave
+      // 
+      this.btnSave.Location = new System.Drawing.Point(318, 407);
+      this.btnSave.Name = "btnSave";
+      this.btnSave.Size = new System.Drawing.Size(306, 23);
+      this.btnSave.TabIndex = 6;
+      this.btnSave.Text = "button1";
+      this.btnSave.UseVisualStyleBackColor = true;
+      this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(636, 442);
+      this.Controls.Add(this.btnSave);
       this.Controls.Add(this.txtFullName);
       this.Controls.Add(this.lblFullName);
       this.Controls.Add(this.btnAdd);
@@ -90,6 +102,7 @@
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Label lblFullName;
         private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.Button btnSave;
     }
 }
 
--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,7 @@ namespace UserMaintenance
       InitializeComponent();
       lblFullName.Text = Resource1.FullName; // label1
       btnAdd.Text = Resource1.Add; // button1
+      btnSave.Text = Resource1.Save;
 
       listUsers.DataSource = users;
       listUsers.ValueMember = "ID";
@@ -33,6 +35,21 @@ namespace UserMaintenance
         FullName = txtFullName.Text,
       };
       users.Add(u);
+    }
+
+    private void btnSave_Click(object sender, EventArgs e)
+    {
+      var sfd = new SaveFileDialog();
+      if (sfd.ShowDialog() == DialogResult.OK)
+      {
+        using (StreamWriter sw = new StreamWriter(sfd.FileName))
+        {
+          foreach (User item in listUsers.Items)
+          {
+            sw.WriteLine(item.FullName);
+          }
+        }
+      }
     }
   }
 }
